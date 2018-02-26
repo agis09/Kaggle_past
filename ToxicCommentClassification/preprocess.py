c6e3e2c7a5f0422b62e650e4252fb31be06bb17c
agis09 <33488378+agis09@users.noreply.github.com>
2018-02-26 22:08:43 +0900

    Add files via upload

---
+++ ToxicCommentClassification/preprocess.py
@@ -0,0 +1,137 @@
+import numpy as np
+import pandas as pd
+import re
+train = pd.read_csv("train.csv", encoding="utf-8")
+test = pd.read_csv("test.csv", encoding="utf-8")
+
+
+repl = {
+    "&lt;3": " good ",
+    ":d": " good ",
+    ":dd": " good ",
+    ":p": " good ",
+    "8)": " good ",
+    ":-)": " good ",
+    ":)": " good ",
+    ";)": " good ",
+    "(-:": " good ",
+    "(:": " good ",
+    "yay!": " good ",
+    "yay": " good ",
+    "yaay": " good ",
+    "yaaay": " good ",
+    "yaaaay": " good ",
+    "yaaaaay": " good ",
+    ":/": " bad ",
+    ":&gt;": " sad ",
+    ":')": " sad ",
+    ":-(": " bad ",
+    ":(": " bad ",
+    ":s": " bad ",
+    ":-s": " bad ",
+    "&lt;3": " heart ",
+    ":d": " smile ",
+    ":p": " smile ",
+    ":dd": " smile ",
+    "8)": " smile ",
+    ":-)": " smile ",
+    ":)": " smile ",
+    ";)": " smile ",
+    "(-:": " smile ",
+    "(:": " smile ",
+    ":/": " worry ",
+    ":&gt;": " angry ",
+    ":')": " sad ",
+    ":-(": " sad ",
+    ":(": " sad ",
+    ":s": " sad ",
+    ":-s": " sad ",
+    r"\br\b": "are",
+    r"\bu\b": "you",
+    r"\bhaha\b": "ha",
+    r"\bhahaha\b": "ha",
+    r"\bdon't\b": "do not",
+    r"\bdoesn't\b": "does not",
+    r"\bdidn't\b": "did not",
+    r"\bhasn't\b": "has not",
+    r"\bhaven't\b": "have not",
+    r"\bhadn't\b": "had not",
+    r"\bwon't\b": "will not",
+    r"\bwouldn't\b": "would not",
+    r"\bcan't\b": "can not",
+    r"\bcannot\b": "can not",
+    r"\bi'm\b": "i am",
+    "m": "am",
+    "r": "are",
+    "u": "you",
+    "haha": "ha",
+    "hahaha": "ha",
+    "don't": "do not",
+    "doesn't": "does not",
+    "didn't": "did not",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "hadn't": "had not",
+    "won't": "will not",
+    "wouldn't": "would not",
+    "can't": "can not",
+    "cannot": "can not",
+    "i'm": "i am",
+    "m": "am",
+    "i'll" : "i will",
+    "its" : "it is",
+    "it's" : "it is",
+    "'s" : " is",
+    "that's" : "that is",
+    "weren't" : "were not",
+}
+
+keys = [i for i in repl.keys()]
+
+new_train_data = []
+new_test_data = []
+ltr = train["comment_text"].tolist()
+lte = test["comment_text"].tolist()
+
+for i in ltr:
+    arr = str(i).split()
+    xx = ""
+    for j in arr:
+        j = str(j).lower()
+        if j[:4] == 'http' or j[:3] == 'www':
+            continue
+        if j in keys:
+            # print("inn")
+            j = repl[j]
+        xx += j + " "
+    new_train_data.append(xx)
+for i in lte:
+    arr = str(i).split()
+    xx = ""
+    for j in arr:
+        j = str(j).lower()
+        if j[:4] == 'http' or j[:3] == 'www':
+            continue
+        if j in keys:
+            # print("inn")
+            j = repl[j]
+        xx += j + " "
+    new_test_data.append(xx)
+
+train["new_comment_text"] = new_train_data
+test["new_comment_text"] = new_test_data
+
+trate = train["new_comment_text"].tolist()
+tete = test["new_comment_text"].tolist()
+
+for i, c in enumerate(trate):
+    trate[i] = re.sub('[^a-zA-Z ?!]+', '', str(trate[i]).lower())
+for i, c in enumerate(tete):
+    tete[i] = re.sub('[^a-zA-Z ?!]+', '', tete[i])
+
+train["comment_text"]=trate
+test["comment_text"]=tete
+
+
+train.to_csv("train_preprocessed.csv", index=False)
+test.to_csv("test_preprocessed.csv", index=False)
