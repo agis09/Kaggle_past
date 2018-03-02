cb66c2a368bb298751b4bfcf076f66a3c29969da
agis09 <33488378+agis09@users.noreply.github.com>
2018-03-02 21:12:43 +0900

    Update words_train.py

--- ToxicCommentClassification/words_train.py
+++ ToxicCommentClassification/words_train.py
@@ -1,119 +1,94 @@
 import numpy as np
 import pandas as pd
-from keras.models import Model
-from keras.layers import Dense, Embedding, Input,GRU
-from keras.layers import Conv1D, MaxPooling1D, GlobalMaxPool1D, Dropout, concatenate,Flatten
-from keras.preprocessing import text, sequence
 from sklearn.model_selection import train_test_split
-from keras.optimizers import Adam
+from keras.layers import Input, Conv1D, Dense, MaxPool1D, Flatten, Dropout
+from keras.models import Sequential
+from keras.models import Model
 from keras import backend as K
+from keras.layers import Activation
 from keras.layers.normalization import BatchNormalization
+from keras.models import model_from_json
+import matplotlib.pyplot as plt
+from keras.layers.normalization import BatchNormalization
+import csv
+from keras.optimizers import Adam
 
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+df = pd.read_csv("test_preprocessed.csv", encoding="utf-8")
 
-max_features = 20000
-maxlen = 100
+data = df.loc[:, "comment_text"].as_matrix().astype('str')
 
-df = pd.read_csv("train.csv", encoding="utf-8")
-df2 = df[(df["toxic"] > 0) | (df["severe_toxic"] > 0) | (df["obscene"] > 0) | \
-        (df["threat"] > 0) | (df["insult"] > 0) | (df["identity_hate"] > 0)]
-
-data = df2.loc[:, "comment_text"].fillna("fillna")
-data = data.as_matrix().astype('str')
-labels = df2.loc[:, "toxic":"identity_hate"]
-labels = labels.as_matrix().astype('float16')
-
-np.random.permutation(df.index)
-df0 = df[(df["toxic"] == 0) & (df["severe_toxic"] == 0) & (df["obscene"] == 0) & \
-        (df["threat"] == 0) & (df["insult"] == 0) & (df["identity_hate"] == 0)].reset_index(drop=True)
-
-data0 = df0.loc[:len(data)//2,"comment_text"].fillna("fillna")
-data0 = data0.as_matrix().astype('str')
-labels0 = df0.loc[:len(data)//2, "toxic":"identity_hate"]
-labels0 = labels0.as_matrix().astype('float16')
+df_s = pd.read_csv("sample_submission.csv", encoding="utf-8")
+df_s.to_csv("predict.csv", index=False)
 
 
-# print(labels)
-leng = len(labels)+len(labels0)
-list_sentences_train = np.append(data, data0)
-y = np.append(labels, labels0)
-y = np.reshape(y, (leng, 6))
-# print(y)
+input_len = 300
+CLASSES_LIST = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+
+def make_one_hot(char):
+    index = ord(char) - ord("a")
+    value = np.zeros(27)
+    if index >= 0 and index <= 25:
+        value[index] = 1.0
+    else:
+        value[26] = 1.0
+    return value
 
 
-list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+def make_input(x):
+    lst = np.array([])
+    for words in x:
+        words = words.lower()
+        tmp = np.array([])
+        cnt = input_len
+        for word in words:
+            tmp = np.append(tmp, [make_one_hot(char) for char in word])
+            cnt -= 1
+            if cnt == 0:
+                break
+        if cnt > 0:
+            for i in range (0, cnt):
+                tmp = np.append(tmp, np.zeros(27))
+        tmp = np.reshape(tmp, (input_len, 27))
+        lst = np.append(lst, tmp)
+    lst = np.reshape(lst, (len(x), input_len, 27))
+    return lst
 
-tokenizer = text.Tokenizer(num_words=max_features,
-                           filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
-                           lower=True,
-                           split=" ",
-                           char_level=False
-                           )
-tokenizer.fit_on_texts(list(list_sentences_train))
 
-# train data
-list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
-X_t = sequence.pad_sequences(list_tokenized_train, maxlen=maxlen)
+def batch_iter(data_set, batch_size, shuffle=False):
+    num_batches_per_epoch = int((len(data_set)-1)/batch_size)+1
+
+    def data_generator():
+        data_size = len(data_set)
+        while True:
+            if shuffle:
+                shuffle_indices = np.random.permutation(np.arange(data_size))
+                shuffled_data = data_set[shuffle_indices]
+            else:
+                shuffled_data = data_set
+
+            for batch_num in range(num_batches_per_epoch):
+                start_index = batch_num*batch_size
+                end_index = min((batch_num+1)*batch_size, data_size)
+                x = shuffled_data[start_index:end_index]
+                yield make_input(x)
+    return num_batches_per_epoch, data_generator()
 
 
-def weight_variable(shape):
-    return K.truncated_normal(shape, stddev=0.01)
 
+for i in range(6):
+    model = model_from_json(open('model_' + CLASSES_LIST[i] + '.json').read())
 
-def cnn_rnn():
-    embed_size = 256
-    inp = Input(shape=(maxlen, ))
-    main = Embedding(max_features, embed_size)(inp)
-    main = Dropout(0.2)(main)
-    main = Conv1D(filters=32, kernel_size=2, padding='same', activation='relu')(main)
-    main = MaxPooling1D(pool_size=2)(main)
-    main = Conv1D(filters=32, kernel_size=2, padding='same', activation='relu')(main)
-    main = MaxPooling1D(pool_size=2)(main)
-
-    main = GRU(32)(main)
-    main = Dense(16, activation="relu")(main)
-    main = Dense(6, activation="sigmoid")(main)
-    model = Model(inputs=inp, outputs=main)
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    return model
-
-model = cnn_rnn()
-model.summary()
-
-model.compile(loss='binary_crossentropy',
+    model.compile(loss='binary_crossentropy',
                   optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999),
                   metrics=['accuracy'])
-#############################train#######################################
 
-X_t_train, X_t_test, y_train, y_test = train_test_split(X_t, y, test_size = 0.10)
+    model.load_weights('w_' + CLASSES_LIST[i] + '.hdf5')
 
-batch_size = 128
-epochs = 2
+    batch_size = 1
+    steps, generator = batch_iter(data, batch_size)
 
-model.fit(X_t_train, y_train,
-          validation_data=(X_t_test, y_test),
-          batch_size=batch_size,
-          epochs=epochs,
-          shuffle = True)
-
-model.save_weights("test.h5")
-model.save('words_model.h5')
-
-
-##############################predict##########################
-
-test = pd.read_csv("test.csv",encoding="utf-8")
-list_sentences_test = test["comment_text"].fillna("unknown").values
-
-
-# test data
-list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
-X_te = sequence.pad_sequences(list_tokenized_test, maxlen=maxlen)
-
-model.load_weights("test.h5")
-
-y_test = model.predict(X_te,verbose=1)
-sample_submission = pd.read_csv("sample_submission.csv")
-sample_submission[list_classes] = y_test
-sample_submission.to_csv("predictions.csv", index=False)
+    hist = model.predict_generator(generator, steps, verbose=1)
+    
+    df2 = pd.read_csv("predict.csv", encoding="utf-8")
+    df2[CLASSES_LIST[i]] = hist
+    df2.to_csv("predict.csv", index=False)
