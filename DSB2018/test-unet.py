d6139eda098e881401f6ae5bdfe98312c1127182
agis09 <33488378+agis09@users.noreply.github.com>
2018-03-29 15:14:56 +0900

    Update test-unet.py

--- DSB2018/test-unet.py
+++ DSB2018/test-unet.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import cv2
 from tqdm import tqdm
 from itertools import chain
-from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.io import imread, imshow, imread_collection, concatenate_images,imsave
 from skimage import color,exposure
 from skimage.transform import resize
 from skimage.morphology import label
@@ -25,10 +25,17 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import backend as K
 from keras.optimizers import Adam
 import tensorflow as tf
+from keras.models import Model
+from keras.layers import Input
+from keras.layers.convolutional import Conv2D, ZeroPadding2D, Conv2DTranspose
+from keras.layers.merge import concatenate
+from keras.layers import LeakyReLU, BatchNormalization, Activation, Dropout
 
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 IMG_CHANNELS = 3
+
+IMG_SIZE = 256
 
 TRAIN_PATH = 'stage1_train/'
 TEST_PATH = 'stage1_test/'
@@ -40,14 +47,36 @@ np.random.seed = seed
 train_ids = next(os.walk(TRAIN_PATH))[1]
 test_ids = next(os.walk(TEST_PATH))[1]
 
-X_train = np.zeros((len(train_ids)*5, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
-Y_train = np.zeros((len(train_ids)*5, IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+X_train = np.zeros((len(train_ids)*5, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.double)
+Y_train = np.zeros((len(train_ids)*5, IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.double)
+
+# 値を-1から1に正規化する関数
+def normalize_x(image):
+    image = image/127.5 - 1
+    return image
+
+
+# 値を0から1に正規化する関数
+def normalize_y(image):
+    image = image/255
+    return image
+
+
+# 値を0から255に戻す関数
+def denormalize_y(image):
+    image = image*255
+    return image
+
+
 print('Getting and resizing train images and masks ... ')
 sys.stdout.flush()
 for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
     path = TRAIN_PATH + id_
     img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
     img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+
+    img = normalize_x(img)
+
     X_train[n] = img
     X_train[n+len(train_ids)]=np.rot90(img)
     X_train[n+len(train_ids)*2]=np.rot90(img,2)
@@ -61,16 +90,20 @@ for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
                                       preserve_range=True), axis=-1)
         mask = np.maximum(mask, mask_)
 
+    mask=normalize_y(mask)
+
     Y_train[n] = mask
     Y_train[n+len(train_ids)]=np.rot90(mask)
     Y_train[n+len(train_ids)*2]=np.rot90(mask,2)
     mask=np.reshape(cv2.flip(mask,1),(IMG_HEIGHT,IMG_WIDTH,1))
     Y_train[n+len(train_ids)*3]=np.rot90(mask,3)
     Y_train[n+len(train_ids)*4]=mask
+
+
 np.save("X_train",X_train)
 np.save("Y_train",Y_train)
 
-X_test = np.zeros((len(test_ids)*1, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.double)
 sizes_test = []
 print('Getting and resizing test images ... ')
 sys.stdout.flush()
@@ -79,6 +112,8 @@ for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
     img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
     sizes_test.append([img.shape[0], img.shape[1]])
     img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    img = normalize_x(img)
+
     X_test[n] = img
 
 
@@ -173,76 +208,143 @@ def dice_coef_loss(y_true, y_pred):
 
 
 
+class UNet(object):
+    def __init__(self, input_channel_count, output_channel_count, first_layer_filter_count):
+        self.INPUT_IMAGE_SIZE = 256
+        self.CONCATENATE_AXIS = -1
+        self.CONV_FILTER_SIZE = 4
+        self.CONV_STRIDE = 2
+        self.CONV_PADDING = (1, 1)
+        self.DECONV_FILTER_SIZE = 2
+        self.DECONV_STRIDE = 2
 
-# Build U-Net model
-inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
-s = Lambda(lambda x: x / 255) (inputs)
+        # (256 x 256 x input_channel_count)
+        inputs = Input((self.INPUT_IMAGE_SIZE, self.INPUT_IMAGE_SIZE, input_channel_count))
 
-c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (s)
-c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (c1)
-p1 = MaxPooling2D((2, 2)) (c1)
+        # エンコーダーの作成
+        # (128 x 128 x N)
+        enc1 = ZeroPadding2D(self.CONV_PADDING)(inputs)
+        enc1 = Conv2D(first_layer_filter_count, self.CONV_FILTER_SIZE, strides=self.CONV_STRIDE)(enc1)
 
-c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (p1)
-c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (c2)
-p2 = MaxPooling2D((2, 2)) (c2)
+        # (64 x 64 x 2N)
+        filter_count = first_layer_filter_count*2
+        enc2 = self._add_encoding_layer(filter_count, enc1)
 
-c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (p2)
-c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (c3)
-p3 = MaxPooling2D((2, 2)) (c3)
+        # (32 x 32 x 4N)
+        filter_count = first_layer_filter_count*4
+        enc3 = self._add_encoding_layer(filter_count, enc2)
 
-c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (p3)
-c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (c4)
-p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+        # (16 x 16 x 8N)
+        filter_count = first_layer_filter_count*8
+        enc4 = self._add_encoding_layer(filter_count, enc3)
 
-c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (p4)
-c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (c5)
+        # (8 x 8 x 8N)
+        enc5 = self._add_encoding_layer(filter_count, enc4)
 
-u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c5)
-u6 = concatenate([u6, c4])
-c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (u6)
-c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (c6)
+        # (4 x 4 x 8N)
+        enc6 = self._add_encoding_layer(filter_count, enc5)
 
-u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c6)
-u7 = concatenate([u7, c3])
-c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (u7)
-c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (c7)
+        # (2 x 2 x 8N)
+        enc7 = self._add_encoding_layer(filter_count, enc6)
 
-u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c7)
-u8 = concatenate([u8, c2])
-c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (u8)
-c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (c8)
+        # (1 x 1 x 8N)
+        enc8 = self._add_encoding_layer(filter_count, enc7)
 
-u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same') (c8)
-u9 = concatenate([u9, c1], axis=3)
-c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (u9)
-c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (c9)
+        # デコーダーの作成
+        # (2 x 2 x 8N)
+        dec1 = self._add_decoding_layer(filter_count, True, enc8)
+        dec1 = concatenate([dec1, enc7], axis=self.CONCATENATE_AXIS)
 
-outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+        # (4 x 4 x 8N)
+        dec2 = self._add_decoding_layer(filter_count, True, dec1)
+        dec2 = concatenate([dec2, enc6], axis=self.CONCATENATE_AXIS)
 
-model = Model(inputs=[inputs], outputs=[outputs])
-# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[my_iou_metric])
+        # (8 x 8 x 8N)
+        dec3 = self._add_decoding_layer(filter_count, True, dec2)
+        dec3 = concatenate([dec3, enc5], axis=self.CONCATENATE_AXIS)
+
+        # (16 x 16 x 8N)
+        dec4 = self._add_decoding_layer(filter_count, False, dec3)
+        dec4 = concatenate([dec4, enc4], axis=self.CONCATENATE_AXIS)
+
+        # (32 x 32 x 4N)
+        filter_count = first_layer_filter_count*4
+        dec5 = self._add_decoding_layer(filter_count, False, dec4)
+        dec5 = concatenate([dec5, enc3], axis=self.CONCATENATE_AXIS)
+
+        # (64 x 64 x 2N)
+        filter_count = first_layer_filter_count*2
+        dec6 = self._add_decoding_layer(filter_count, False, dec5)
+        dec6 = concatenate([dec6, enc2], axis=self.CONCATENATE_AXIS)
+
+        # (128 x 128 x N)
+        filter_count = first_layer_filter_count
+        dec7 = self._add_decoding_layer(filter_count, False, dec6)
+        dec7 = concatenate([dec7, enc1], axis=self.CONCATENATE_AXIS)
+
+        # (256 x 256 x output_channel_count)
+        dec8 = Activation(activation='relu')(dec7)
+        dec8 = Conv2DTranspose(output_channel_count, self.DECONV_FILTER_SIZE, strides=self.DECONV_STRIDE)(dec8)
+        dec8 = Activation(activation='sigmoid')(dec8)
+
+        self.UNET = Model(input=inputs, output=dec8)
+
+    def _add_encoding_layer(self, filter_count, sequence):
+        new_sequence = LeakyReLU(0.2)(sequence)
+        new_sequence = ZeroPadding2D(self.CONV_PADDING)(new_sequence)
+        new_sequence = Conv2D(filter_count, self.CONV_FILTER_SIZE, strides=self.CONV_STRIDE)(new_sequence)
+        new_sequence = BatchNormalization()(new_sequence)
+        return new_sequence
+
+    def _add_decoding_layer(self, filter_count, add_drop_layer, sequence):
+        new_sequence = Activation(activation='relu')(sequence)
+        new_sequence = Conv2DTranspose(filter_count, self.DECONV_FILTER_SIZE, strides=self.DECONV_STRIDE,
+                                       kernel_initializer='he_uniform')(new_sequence)
+        new_sequence = BatchNormalization()(new_sequence)
+        if add_drop_layer:
+            new_sequence = Dropout(0.5)(new_sequence)
+        return new_sequence
+
+    def get_model(self):
+        return self.UNET
+
+
+# 入力はBGR3チャンネル
+input_channel_count = 3
+# 出力はグレースケール1チャンネル
+output_channel_count = 1
+# 一番初めのConvolutionフィルタ枚数は64
+first_layer_filter_count = 64
+
+network = UNet(input_channel_count, output_channel_count, first_layer_filter_count)
+model = network.get_model()
+# model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=[my_iou_metric])
 model.compile(loss=dice_coef_loss, optimizer=Adam(), metrics=[dice_coef])
-model.summary()
 
-# Fit model
-# earlystopper = EarlyStopping(patience=5, verbose=1)
-checkpointer = ModelCheckpoint('model-dsbowl2018-1.h5', verbose=1, save_best_only=True)
-results = model.fit(X_train, Y_train, validation_split=0.2, batch_size=8, epochs=30,verbose=1)
+BATCH_SIZE = 12
+NUM_EPOCH = 20
+# history = model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=NUM_EPOCH, verbose=1)
+# #model.save_weights('unet_weights.hdf5')
 
 
 #############  predict  ###############
 
-# model = load_model('model-dsbowl2018-1.h5',custom_objects={'my_iou_metric': my_iou_metric})
-model = load_model('model-dsbowl2018-1.h5',custom_objects={'dice_coef': dice_coef})
-preds_test = model.predict(X_test, verbose=1)
+model.load_weights('unet_weights.hdf5')
+preds_test = model.predict(X_test,verbose=1)
 
-
-preds_test_t = (preds_test > 0.5).astype(np.uint8)
 preds_test_upsampled = []
+
+for i in range(10):
+    tmp = X_test[i]
+    tmp = np.reshape(tmp,(256,256))
+    imsave("mask"+str(i)+"_test.bmp",tmp)
+    tmp=denormalize_y(preds_test[i])
+    tmp = np.reshape(tmp,(256,256))
+    imsave("mask"+str(i)+"_pred.bmp",tmp)
+
 for i in range(len(preds_test)):
-    preds_test_upsampled.append(resize(np.squeeze(preds_test[i]),
-                                       (sizes_test[i][0], sizes_test[i][1]),
-                                       mode='constant', preserve_range=True))
+    preds_test_upsampled.append(denormalize_y(resize(np.squeeze(preds_test[i]),
+                                       (sizes_test[i][0], sizes_test[i][1]), mode='constant', preserve_range=True)))
 
 
 def rle_encoding(x):
@@ -268,6 +370,7 @@ for n, id_ in enumerate(test_ids):
     rles.extend(rle)
     new_test_ids.extend([id_] * len(rle))
 
+print(len(set(new_test_ids)))
 sub = pd.DataFrame()
 sub['ImageId'] = new_test_ids
 sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
